92c1fa72da9a45b0ffd840824691f1a4797e2772
Joe Pearson <pearjo@protonmail.com>
2020-02-13 21:43:47 +0100

    license: Add the GNU GPL v3 license

--- scripts/knut.py
+++ scripts/knut.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+"""
+Copyright (C) 2020  Joe Pearson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
 from knutapis import Light
 from knutapis import Temperature
 from knutserver import KnutTcpSocket
@@ -55,6 +72,13 @@ def load_service_backend(config, service):
 
 
 def main():
+    print(
+        'knut.py  Copyright (C) 2020  Joe Pearson\n'
+        'This program comes with ABSOLUTELY NO WARRANTY; for details read LICENSE.\n'
+        'This is free software, and you are welcome to redistribute it\n'
+        'under certain conditions; read LICENSE for details.'
+    )
+
     parser = argparse.ArgumentParser(description='TCP server example.')
     parser.add_argument('--log', dest='logLevel', choices=LOGLEVELS,
                         help='Set the logging level')
