36f3bf7dbff626774574837ab94206f9e8e0a982
Joe Pearson <pearjo@protonmail.com>
2020-03-01 14:22:02 +0100

    tcpsocket: Fix outgoing messages
    Various fixes to ensure that all outgoing messanges are
    send.

--- knutserver/tcpsocket.py
+++ knutserver/tcpsocket.py
@@ -74,7 +74,7 @@ class KnutTcpSocket():
     incoming request by the client and to send a proper response.
     """
     def __init__(self, host='localhost', port=8080):
-        logging.info('Open server on socket %s:%i...' % (host, port))
+        logging.info(str('Open server on socket %s:%i...' % (host, port)))
         self.services = dict()
         self.addr = (host, port)
         self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -85,8 +85,7 @@ class KnutTcpSocket():
         self._in_sockets = [self.serversocket]  # sockets to read from
         self._out_sockets = list()  # sockets to write to
         self._out_msg_queues = dict()  # outgoing message queues
-        self._ready_in_sockets = list()  # sockets that are actually readyble
-        self._ready_out_sockets = list()  # sockets that are actually writable
+        self._known_clients = list()  # client sockets which might be open
 
         listener_thread = threading.Thread(target=self.listener,
                                            name='listener_thread')
@@ -113,39 +112,42 @@ class KnutTcpSocket():
     def listener(self):
         """Listen for connections and manages in- and outgoing messages."""
         while self._in_sockets:
-            logging.info('Server is listening for connections...')
-            self._ready_in_sockets, self._ready_out_sockets, error_sockets = \
+            logging.info('Server is waiting for events...')
+            ready_in_sockets, ready_out_sockets, error_sockets = \
                 select.select(self._in_sockets,
                               self._out_sockets,
                               self._in_sockets)
 
             # input handler
-            for in_socket in self._ready_in_sockets:
+            for in_socket in ready_in_sockets:
                 self._input_socket_handler(in_socket)
 
             # output handler
-            for out_socket in self._ready_out_sockets:
+            for out_socket in ready_out_sockets:
                 self._output_socket_handler(out_socket)
 
             # error handler
             for in_error in error_sockets:
-                logging.debug('Cleanup socket %s with error...'
-                              % str(in_error.getpeername()))
+                logging.debug(str('Cleanup socket %s with error...'
+                                  % str(in_error.getpeername())))
                 self._in_sockets.remove(in_error)
                 if in_error in self._out_sockets:
                     self._out_sockets.remove(in_error)
 
                 in_error.close()
-                del self._out_msg_queues[self.in_error]
+                del self._out_msg_queues[in_error]
 
     def _input_socket_handler(self, in_socket):
         """Handles an input socket *in_socket*."""
         if in_socket is self.serversocket:
             clientsocket, clientaddr = in_socket.accept()
+            logging.debug(str('Accepted connection from %s.'
+                              % str(clientaddr)))
             clientsocket.setblocking(0)
 
-            # add socket to input sockets
+            # add socket to input sockets and know clients
             self._in_sockets.append(clientsocket)
+            self._known_clients.append(clientsocket)
 
             # queue for data to send
             self._out_msg_queues[clientsocket] = queue.Queue()
@@ -160,7 +162,8 @@ class KnutTcpSocket():
                 if in_socket not in self._out_sockets:
                     self._out_sockets.append(in_socket)
             else:
-                self._in_sockets.remove(in_socket)
+                if in_socket in self._in_sockets:
+                    self._in_sockets.remove(in_socket)
                 if in_socket in self._out_sockets:
                     self._out_sockets.remove(in_socket)
 
@@ -173,18 +176,17 @@ class KnutTcpSocket():
         try:
             # get next message from the queue
             next_msg = self._out_msg_queues[out_socket].get_nowait()
-            logging.debug('Send %s to %s...' % (next_msg,
-                                                str(out_socket.getpeername())))
+            logging.debug(str('Send message to %s...'
+                              % str(out_socket.getpeername())))
             out_socket.sendall(next_msg)
         except queue.Empty:
-            logging.debug('Message queue for socket %s is empty.'
-                          % str(out_socket.getpeername()))
-            pass
-        except (KeyError,
-                OSError):
-            logging.debug('Remove output socket...')
+            logging.debug(str('Message queue for socket %s is empty.'
+                              % str(out_socket.getpeername())))
             self._out_sockets.remove(out_socket)
-            self._ready_out_sockets.remove(out_socket)
+        except (KeyError, OSError):
+            if out_socket in self._out_sockets:
+                logging.debug('Remove output socket...')
+                self._out_sockets.remove(out_socket)
 
     def client_reader(self, clientsocket):
         """Returns a Knut response message upon a clients message.
@@ -194,7 +196,7 @@ class KnutTcpSocket():
         is then returned as Knut message.
         """
         byte_data = bytearray()
-        byte_response = bytearray()
+        byte_response = None
         data = dict()
         response = dict()
         response_id = 0x0000
@@ -203,38 +205,32 @@ class KnutTcpSocket():
             # read the first to message size bytes to check if connection
             # is open
             msg_size_bytes = clientsocket.recv(4)
-
-            # if no header is read, return none
-            if not msg_size_bytes:
-                return
-
             msg_size = int.from_bytes(msg_size_bytes, byteorder='big')
-            logging.debug('Received %i bytes.' % msg_size)
+            logging.debug(str('Received %i bytes.' % msg_size))
 
             if msg_size > 0:
                 byte_data = clientsocket.recv(msg_size)
-                logging.debug('Received raw bytes %s.'
-                              % str(byte_data))
+                logging.debug(str('Received raw bytes %s.'
+                                  % str(byte_data)))
                 data = json.loads(byte_data, encoding=ENCODING)
                 logging.debug(str('Received ' + str(data)))
 
                 msg_id = data['msgId']
                 service_id = data['serviceId']
 
-                logging.debug('Received message id %i for service %i.'
-                              % (msg_id, service_id))
+                logging.debug(str('Received message id %i for service %i.'
+                                  % (msg_id, service_id)))
 
                 response_id, response = self.request_handler(service_id,
                                                              msg_id,
                                                              data['msg'])
-        except (json.decoder.JSONDecodeError,
-                ValueError,
-                ConnectionResetError,
-                TypeError):
+        except json.decoder.JSONDecodeError:
             logging.warning('Failed to decode JSON message.')
+        except ConnectionResetError:
+            logging.debug('Connection reset by peer...')
 
         if response_id > 0:
-            byte_response = self.msg_builder(service_id, response_id, response)
+            byte_response = msg_builder(service_id, response_id, response)
 
         return byte_response
 
@@ -246,37 +242,47 @@ class KnutTcpSocket():
         this method.
         """
         if service_id not in self.services.keys():
-            logging.warning('No known service \'%s\'.' % hex(service_id))
+            logging.warning(str('No known service \'%s\'.' % hex(service_id)))
             return msg_id, payload
 
         return self.services[service_id].request_handler(msg_id, payload)
 
     def send(self, service_id, msg_id, msg):
         """Sends the message *msg* to all open sockets."""
-        byte_msg = self.msg_builder(service_id, msg_id, msg)
+        byte_msg = msg_builder(service_id, msg_id, msg)
 
-        for out_socket in self._ready_out_sockets:
-            self._out_msg_queues[out_socket].put(byte_msg)
-            self._output_socket_handler(out_socket)
-
-    def msg_builder(self, service_id, msg_id, msg):
-        """Returns *msg* as Knut message with header as byte array."""
-        data = dict()
-
-        data['serviceId'] = service_id
-        data['msgId'] = msg_id
-        data['msg'] = msg
-
-        data_str = json.dumps(data)
-        msg_size = len(data_str).to_bytes(4, byteorder='big')
-
-        logging.debug('Build %i byte long message %s.'
-                      % (len(data_str), str(data_str)))
-
-        return msg_size + bytearray(data_str, ENCODING)
+        for client in self._known_clients:
+            try:
+                logging.debug(str('Send message to %s...'
+                                  % str(client.getpeername())))
+                client.sendall(byte_msg)
+            except OSError:
+                self._known_clients.remove(client)
+                client.close()
 
     def exit(self):
         """Close the server socket."""
         logging.info('Close server socket.')
-        self.serversocket.close()
-        self._in_sockets.remove(self.serversocket)
+
+        for in_socket in self._in_sockets:
+            in_socket.close()
+
+        for out_socket in self._out_sockets:
+            out_socket.close()
+
+
+def msg_builder(service_id, msg_id, msg):
+    """Returns *msg* as Knut message with header as byte array."""
+    data = dict()
+
+    data['serviceId'] = service_id
+    data['msgId'] = msg_id
+    data['msg'] = msg
+
+    data_str = json.dumps(data)
+    msg_size = len(data_str).to_bytes(4, byteorder='big')
+
+    logging.debug(str('Build %i byte long message %s.'
+                      % (len(data_str), str(data_str))))
+
+    return msg_size + bytearray(data_str, ENCODING)
